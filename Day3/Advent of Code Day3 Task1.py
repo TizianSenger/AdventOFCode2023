4ade591a5f28d25c7b3f21372d11d0a053989eb2
TizianSenger <tiziansengerbw@gmail.com>
2023-12-03 15:59:38 +0100

    asdf

--- Day3/Advent of Code Day3 Task1.py
+++ Day3/Advent of Code Day3 Task1.py
@@ -1,9 +1,9 @@
 #   Advent of Code 3
 contentList = []
-
 def readCSVFile():
     with open('data.csv', 'r') as file:
         for row in file:
             contentList.append(row.strip())
+    print(contentList)
 
 readCSVFile()
